3c087935de9c59f223febaf5167cf9ca017927f2
dataDuchess <kpokrass2@gmail.com>
2024-02-19 22:39:22 -0500

    unpack game-level stats zip archive

--- src/unpack_game_level_stats_files.py
+++ src/unpack_game_level_stats_files.py
@@ -1,12 +1,11 @@
-# Utility script to unzip and organize hockey stats files from Kaggle
+# Utility script to unzip and organize game-level hockey stats files from Kaggle
 from zipfile import ZipFile
 
 # Define variables
 DATA_FOLDER = '../data/raw/'
-FILE_NAME = 'archive.zip'
+FILE_NAME = 'NHL_game-Level.zip'
 OUTPUT_FOLDER = '../data/interim/'
 
 # Unzip archive folder
 with ZipFile(DATA_FOLDER + FILE_NAME, 'r') as zObject:
     zObject.extractall(path=OUTPUT_FOLDER)
-
